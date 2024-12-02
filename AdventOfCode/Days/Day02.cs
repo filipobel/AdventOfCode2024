0799755c9eccce5ead76e051a2bbe0d77b1c1dae
Filip Obel <filipobel@hotmail.com>
2024-12-02 15:06:14 +0100

    Complete Day 2

---
+++ AdventOfCode/Days/Day02.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days;
+
+public class Day02 : TestableBaseDay
+{
+    private readonly string _input;
+    private List<List<int>> reports = new List<List<int>>();
+    public Day02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+
+        foreach (string line in _input.Split("\n"))
+        {
+            reports.Add(line.Split(" ").Select(s => int.Parse(s)).ToList());
+        }
+    }
+    public override ValueTask<string> Solve_1()
+    {
+        return new (reports.Count(Valid).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int answer = 0;
+        foreach (var report in reports)
+        {
+            if (Valid(report))
+            {  
+                answer++;
+                continue;
+            }
+
+            for (int i = 0; i < report.Count; i++)
+            {
+                List<int> reportToDampen = new(report);
+                reportToDampen.RemoveAt(i);
+
+                if (Valid(reportToDampen))
+                {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return new(answer.ToString());
+    }
+
+    private bool Valid(List<int> report)
+    {
+        var pairs = Enumerable.Zip(report, report.Skip(1));
+        return pairs.All(p => 1 <= p.Second - p.First && p.Second - p.First <= 3) ||
+                pairs.All(p => 1 <= p.First - p.Second && p.First - p.Second <= 3);
+    }
+
+    IEnumerable<int[]> Attenuate(int[] samples) =>
+    from i in Enumerable.Range(0, samples.Length + 1)
+    let before = samples.Take(i - 1)
+    let after = samples.Skip(i)
+    select Enumerable.Concat(before, after).ToArray();
+}
--- AdventOfCode2024Tests/SampleTests.cs
+++ AdventOfCode2024Tests/SampleTests.cs
@@ -1,23 +1,23 @@
 using AdventOfCode.Days;
 using AoCHelper;
 
-namespace AdventOfCode2024Tests
+namespace AdventOfCode2024Tests;
+
+public class SampleTests
 {
-    public class SampleTests
+    [TestCase(typeof(Day01), "11", "31")]
+    [TestCase(typeof(Day02), "2","4")]
+    public async Task Test(Type type, string sol1, string sol2)
     {
-        [TestCase(typeof(Day01), "11", "31")]
-        public async Task Test(Type type, string sol1, string sol2)
+        // Can't use BaseDay since some of them aren't days, but you probably can
+        if (Activator.CreateInstance(type) is BaseProblem instance)
         {
-            // Can't use BaseDay since some of them aren't days, but you probably can
-            if (Activator.CreateInstance(type) is BaseProblem instance)
-            {
-                Assert.AreEqual(sol1, await instance.Solve_1());
-                Assert.AreEqual(sol2, await instance.Solve_2());
-            }
-            else
-            {
-                Assert.Fail($"{type} is not a BaseProblem");
-            }
+            Assert.AreEqual(sol1, await instance.Solve_1());
+            Assert.AreEqual(sol2, await instance.Solve_2());
+        }
+        else
+        {
+            Assert.Fail($"{type} is not a BaseProblem");
         }
     }
 }
