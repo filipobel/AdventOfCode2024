36b4298fd75b6c3be3561ef7b4d08e4c3d380681
filipobel <filipobel@hotmail.com>
2025-01-29 12:49:47 +0100

    Finished Day 13 part 1

---
+++ AdventOfCode/Days/Day13.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days;
+public class Day13 : TestableBaseDay
+{
+    List<ClawMachine> clawMachines;
+    public Day13()
+    {
+        var intialInput = File.ReadAllText(InputFilePath);
+        clawMachines = intialInput.Split("\r\n\r\n").Select(n => new ClawMachine(n)).ToList();
+
+    }
+    public override ValueTask<string> Solve_1()
+    {
+        return new (clawMachines.Sum(x => x.Solve()).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ClawMachine
+{
+    private (int, int) ButtonA;
+    private (int, int) ButtonB;
+    private (int, int) Prize;
+
+    public ClawMachine(String input)
+    {
+        var inputLines = input.Split("\r\n");
+        ButtonA = Regex.Matches(inputLines[0], @"\d+") switch { var a => (int.Parse(a[0].Value), int.Parse(a[1].Value))};
+        ButtonB = Regex.Matches(inputLines[1], @"\d+") switch { var a => (int.Parse(a[0].Value), int.Parse(a[1].Value)) };
+        Prize = Regex.Matches(inputLines[2], @"\d+") switch { var a => (int.Parse(a[0].Value), int.Parse(a[1].Value)) };
+    }
+
+    public long Solve()
+    {
+        if ((ButtonA.Item1 * Prize.Item2 - Prize.Item1 * ButtonA.Item2) % (ButtonA.Item1 * ButtonB.Item2 - ButtonA.Item2 * ButtonB.Item1) != 0) {
+            return 0;
+        }
+        long bPress = (ButtonA.Item1 * Prize.Item2 - Prize.Item1 * ButtonA.Item2) / (ButtonA.Item1 * ButtonB.Item2 - ButtonB.Item1 * ButtonA.Item2);
+
+        if ((Prize.Item1 - bPress * ButtonB.Item1) % ButtonA.Item1 != 0) { return 0; }
+        long a = (Prize.Item1 - bPress * ButtonB.Item1) / ButtonA.Item1;
+
+        return 3*a+ bPress;
+    }
+}
--- AdventOfCode2024Tests/SampleTests.cs
+++ AdventOfCode2024Tests/SampleTests.cs
@@ -17,6 +17,7 @@ public class SampleTests
     [TestCase(typeof(Day10), "36", "81")]
     [TestCase(typeof(Day11), "55312", "65601038650482")]
     [TestCase(typeof(Day12), "1930", "1206")]
+    [TestCase(typeof(Day12), "480", "")]
     public async Task Test(Type type, string sol1, string sol2)
     {
         // Can't use BaseDay since some of them aren't days, but you probably can
